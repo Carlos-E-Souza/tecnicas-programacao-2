942f0786c40e431b0abd1131cc968aa8cbc5ccd6
Carlos-E-Souza <carlos.e.souza2653@gmail.com>
2026-05-20 19:21:02 -0300

    add teste merge ordenado

--- monitoramento-logs/testa_monitora_logs.cpp
+++ monitoramento-logs/testa_monitora_logs.cpp
@@ -30,3 +30,27 @@ TEST_CASE("Lista existe e total ausente cria total_", "[monitora_logs]") {
   REQUIRE(std::getline(total, linha));
   REQUIRE(linha == "20/1/2026 17:45:38 Outro exemplo de log");
 }
+
+TEST_CASE("Lista existe e total existe faz merge ordenado", "[monitora_logs]") {
+  std::remove("./fixtures/total_log_existe.txt");
+  std::ofstream total_base("./fixtures/total_log_existe.txt");
+  REQUIRE(total_base.is_open());
+  total_base << "17/1/2026 14:17:46 Log do total" << "\n";
+  total_base << "21/1/2026 18:55:38 Outro total";
+  total_base.close();
+
+  REQUIRE(processa_lista_logs("./fixtures/lista_com_log_existe.txt"));
+
+  std::ifstream total("./fixtures/total_log_existe.txt");
+  REQUIRE(total.is_open());
+
+  std::string linha;
+  REQUIRE(std::getline(total, linha));
+  REQUIRE(linha == "16/1/2026 13:27:46 Este e um exemplo de log");
+  REQUIRE(std::getline(total, linha));
+  REQUIRE(linha == "17/1/2026 14:17:46 Log do total");
+  REQUIRE(std::getline(total, linha));
+  REQUIRE(linha == "20/1/2026 17:45:38 Outro exemplo de log");
+  REQUIRE(std::getline(total, linha));
+  REQUIRE(linha == "21/1/2026 18:55:38 Outro total");
+}
