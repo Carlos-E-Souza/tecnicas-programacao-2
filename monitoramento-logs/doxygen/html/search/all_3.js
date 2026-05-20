09e6a65c856877c2fcc0f6e0888b672e209928c8
Carlos-E-Souza <carlos.e.souza2653@gmail.com>
2026-05-20 20:42:51 -0300

    doxygen and gcov

---
+++ monitoramento-logs/doxygen/html/search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['processa_5flista_5flogs_0',['processa_lista_logs',['../monitora__logs_8cpp.html#a4ae1ac5d10b3a9de51b985ff83f1231c',1,'processa_lista_logs(char const *caminho_lista):&#160;monitora_logs.cpp'],['../monitora__logs_8hpp.html#a4ae1ac5d10b3a9de51b985ff83f1231c',1,'processa_lista_logs(char const *caminho_lista):&#160;monitora_logs.cpp']]]
+];
---
+++ monitoramento-logs/doxygen/html/search/functions_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['garante_5fdiretorio_5foutput_0',['garante_diretorio_output',['../testa__monitora__logs_8cpp.html#a724a21951c03b2412c0cc0b26236c007',1,'testa_monitora_logs.cpp']]]
+];
